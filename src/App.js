3a2b0a63ea3ed1abc96811f79c867a8b3af7fcbf
benjamesmiller <benjamesmiller@gmail.com>
2018-06-05 14:27:00 -0400

    Added sign-in starting on making ajax resquest to local host

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
+
 
 class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      Username: '',
+      password: '',
+    }
+  }
+
+  pullContacts () {
+    request.get('http://localhost:8000/contacts')
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Contact Manager</h1>
+          </header>
+        <div className="sign-in">
+            <div className="input-field">
+                <label>Username:</label>
+                <input type="text"/>
+            </div>
+
+            <div className="input-field">
+                <label>Password:</label>
+                <input type="password"/>
+            </div>
+
+            <div className="input-field">
+                <label><input type="checkbox"/> Remember me</label>
+            </div>
+    </div>
+    </div>
     );
   }
 }
